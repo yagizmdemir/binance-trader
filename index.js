1f8e910b91d291e103028a2d7b90dd9a3d958d7f
yagizmdemir <yagizmdemir@gmail.com>
2023-03-26 21:13:38 +0300

    telegram

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const http = require("http");
+const TelegramBot = require("node-telegram-bot-api");
+const token = "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8";
+const chatId = -1001944095697;
+const bot = new TelegramBot(token, { polling: false });
 
 const server = http.createServer((req, res) => {
   if (req.method === "POST") {
@@ -10,6 +14,16 @@ const server = http.createServer((req, res) => {
       console.log(new Date());
       const seperated = body.split(" ");
       console.log(seperated);
+
+      bot
+        .sendMessage(chatId, body)
+        .then(() => {
+          console.log("Mesaj gönderildi");
+        })
+        .catch((error) => {
+          console.error("Mesaj gönderilirken hata oluştu:", error);
+        });
+
       res.end("Received and logged request body");
     });
   } else {
