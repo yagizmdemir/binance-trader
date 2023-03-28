41277235b46827ea9c3c16eb2eb00ca420205819
yagizmdemir <yagizmdemir@gmail.com>
2023-03-28 16:23:34 +0300

    updatedHooks

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
+const Binance = require("binance-api-node").default;
+require("dotenv").config;
+
 const app = express();
 const port = 80;
 
+//IP: 138.197.178.218
+
+const client = Binance({
+  apiKey: process.env.BINANCE_API_KEY,
+  apiSecret: process.env.BINANCE_API_KEY_SECRET,
+});
+
 const webhooks = [
+  //Hikmet Ilgaz
   {
     webhook: "lXsibvtpZJ",
     binance: {
-      chatID: -1001944095697,
-      botToken: "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8",
+      chatID: -1001815709655,
+      botToken: "6068843776:AAEktKz3IyEw5URQJyOlZkPtPrAVraKAe-o",
     },
     mt5: {
-      chatID: -1001944095697,
-      botToken: "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8",
+      chatID: -1001815709655,
+      botToken: "6068843776:AAEktKz3IyEw5URQJyOlZkPtPrAVraKAe-o",
     },
   },
+  //Sinem Ilgaz
+  {
+    webhook: "46cR5YwipH",
+    binance: {
+      chatID: -1001715159428,
+      botToken: "6196276189:AAHsnUfNzBri6YKNYC6xgnUM6NK38Qc7Tlc",
+    },
+    mt5: {
+      chatID: -1001715159428,
+      botToken: "6196276189:AAHsnUfNzBri6YKNYC6xgnUM6NK38Qc7Tlc",
+    },
+  },
+  //Yağız Mehmet Demir
   {
     webhook: "IAOuVsVRmR",
     binance: {
@@ -32,6 +56,13 @@ webhooks.map((hook, i) => {
   app.post(`/binance/${hook.webhook}`, (req, res) => {
     const bot = new TelegramBot(hook.binance.botToken, { polling: false });
 
+    //Buy BTCUSD Q=0.01 SL=200 TP=15
+    //Emir tipi [0]
+    //Enstruman [1]
+    //Lot [2].replace('Q=', '')
+    //Stop Loss [3].replace('SL=', '')
+    //Take Profit [4].replace('TP=', '')
+
     let body = "";
     req.on("data", (chunk) => {
       body += chunk.toString();
@@ -40,6 +71,27 @@ webhooks.map((hook, i) => {
       console.log(new Date());
       const seperated = body.split(" ");
       console.log(seperated);
+
+      client
+        .order({
+          symbol: seperated[1].toUpperCase(),
+          side: seperated[0].toUpperCase(),
+          type: "LIMIT",
+          timeInForce: "GTC",
+          quantity: seperated[2].replace('Q=', ''),
+          price: 55000,
+          stopPrice: 54000, // Stop Loss seviyesi
+          icebergQty: 0.001,
+          newOrderRespType: "FULL",
+          stopLimitPrice: 56000, // Take Profit seviyesi
+          type: "STOP_LOSS_LIMIT",
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
 
       bot
         .sendMessage(hook.binance.chatID, body)
@@ -58,9 +110,11 @@ webhooks.map((hook, i) => {
     const bot = new TelegramBot(hook.mt5.botToken, { polling: false });
 
     let body = "";
+    
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
+
     req.on("end", () => {
       bot
         .sendMessage(hook.mt5.chatID, body)
@@ -73,6 +127,7 @@ webhooks.map((hook, i) => {
 
       res.end("Received and logged request body");
     });
+    
   });
 });
 
