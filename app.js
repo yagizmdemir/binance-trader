73ac3b52b50a2940ff993dc4301e493c8d093121
yagizmdemir <yagizmdemir@gmail.com>
2023-04-10 23:45:31 +0300

    first-2

--- app.js
+++ app.js
@@ -62,9 +62,33 @@ const webhooks = [
       botToken: "6196276189:AAHsnUfNzBri6YKNYC6xgnUM6NK38Qc7Tlc",
     },
   },
-  //Yağız Mehmet Demir
+  //Yağız Mehmet Demir - GBPUSD
   {
-    webhook: "IAOuVsVRmR",
+    webhook: "IAOuVsVRmRGBPUSD",
+    binance: {
+      chatID: -1001547297612,
+      botToken: "6005651904:AAFEgtk4xjhHpDU9UiPMu-Az9vdH1PxU8c4",
+    },
+    mt5: {
+      chatID: -1001547297612,
+      botToken: "6005651904:AAFEgtk4xjhHpDU9UiPMu-Az9vdH1PxU8c4",
+    },
+  },
+  //Yağız Mehmet Demir - EURUSD
+  {
+    webhook: "IAOuVsVRmREURUSD",
+    binance: {
+      chatID: -1001948050572,
+      botToken: "6065786579:AAG0wA1jWUy1ly5TqKS-pfU78yTleoBSO9I",
+    },
+    mt5: {
+      chatID: -1001948050572,
+      botToken: "6065786579:AAG0wA1jWUy1ly5TqKS-pfU78yTleoBSO9I",
+    },
+  },
+  //Yağız Mehmet Demir - NZDUSD
+  {
+    webhook: "IAOuVsVRmRNZDUSD",
     binance: {
       chatID: -1001958402970,
       botToken: "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8",
@@ -74,6 +98,18 @@ const webhooks = [
       botToken: "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8",
     },
   },
+  //Yağız Mehmet Demir - AUDUSD
+  {
+    webhook: "IAOuVsVRmRAUDUSD",
+    binance: {
+      chatID: -1001958402970,
+      botToken: "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8",
+    },
+    mt5: {
+      chatID: -1001958402970,
+      botToken: "6209030208:AAEV1yx4r1F03svexoQ73MEg1aP-fRD6rn8",
+    },
+  }
 ];
 
 webhooks.map((hook, i) => {
